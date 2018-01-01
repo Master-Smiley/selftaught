4906eb7b65ca20c7b82ff83869169719c5c10a3d
Garrett Smiley <garrettsmiley@msn.com>
2017-12-31 22:23:59 -0700

    cleaned up guide list and added a show more button

--- src/app/guide/guide-list/guide-list.component.ts
+++ src/app/guide/guide-list/guide-list.component.ts
@@ -11,13 +11,24 @@ export class GuideListComponent implements OnInit {
   guides: Guide[];
   constructor(private guideService: GuideService) { }
 
+  num = 20;
+  guidesLength: number;
+
   ngOnInit() {
+    this.num = 20;
     this.guideService.getGuides()
             .subscribe(
                 (guides: Guide[]) => {
                     this.guides = guides;
+                    this.guidesLength = guides.length;
                 }
             );
   }
+  showMore() {
+    this.num += 20;
+    console.log(this.num);
+    console.log(this.guidesLength);
+  }
+
 }
 
