54a647d3e63bd3fd53cb783d784cc3c0e87e69a0
Garrett Smiley <garrettsmiley@msn.com>
2018-05-19 16:43:57 -0600

    added a render to user route

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,10 +6,6 @@ var User = require('../../models/user');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-router.use(function(req, res, next) {
-    res.render('index');
-    next();
-});
 
 router.post('/signup', function(req, res, next) {
 
