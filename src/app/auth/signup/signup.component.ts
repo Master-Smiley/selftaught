acb40eca39db5c7dd9f432c43b2a3c6bc7e32cb3
Garrett Smiley <garrettsmiley@msn.com>
2018-05-19 23:03:28 -0600

    added validators and fixed url formatting in backend fixed

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -32,7 +32,7 @@ export class SignupComponent implements OnInit {
 
   createForm() {
       this.signupForm = this.formBuilder.group({
-      username: [null, Validators.required, Validators.pattern('[a-z\d\-_\s]+$')],
+      username: [null, Validators.required, Validators.pattern('[a-z\d\-_\s]')],
       email: [null, Validators.compose([Validators.required, CustomValidators.email])],
       password: [null, Validators.compose([Validators.required])],
       re_password:  [null, Validators.compose([Validators.required])]
